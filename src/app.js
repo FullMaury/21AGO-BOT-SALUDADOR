7fdcc36b16ffe0ddef3e2d5f613ca035c694fde5
FullMaury <mauricitogarron@gmail.com>
2025-08-21 09:46:08 -0400

    saludoPorHora

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ const btn = $("#btnGreet");
 const result = $("#result");
 const nameInput = $("#name");
 
+function saludoPorHora(){
+    const h = new Date().getHours();
+    if(h>=5 && h <= 11) return "Buenos dias";
+    if(h >= 12 && h <= 18) return "Buenas tardes";
+    return "Buenas noches";
+}
+
 btn.addEventListener("click", () => {
   const nombre = (nameInput.value || "").trim();
-  result.textContent = nombre ? `Hola ${nombre} ` : "Hola ";
+  const base = saludoPorHora();
+  result.textContent = nombre ? `${base} ${nombre} ` : `${base}`;
 });
